f81438882f6b9af4ffd2aba729d14cc34cc7de2d
HarakalyConsulting <67502460+HarakalyConsulting@users.noreply.github.com>
2020-06-30 19:01:10 +0200

    fixes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,14 +7,14 @@
         <h3>Proprietaire</h3>
         jgfjhgejrfle
         <br>
-        <q-btn color="primary" icon="warning" label="Join" @click="login" class="q-ma-md"></q-btn>
+        <q-btn color="primary" icon="warning" label="Enter" to="owner/login" class="q-ma-md"></q-btn>
 
       </div>
       <div class="col">
         <h3>Locataire</h3>
         jbfkjgebjgkek
         <br>
-        <q-btn color="primary" icon="warning" label="Join" @click="abandon" class="q-ma-md"></q-btn>
+        <q-btn color="primary" icon="warning" label="Enter" to="lodger/login" class="q-ma-md"></q-btn>
 
       </div>
     </div>
