ac47d6d1e02ad13772426753fd909a1adfc0c981
HarakalyConsulting <67502460+HarakalyConsulting@users.noreply.github.com>
2020-06-30 18:48:27 +0200

    action flow (WiP)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,23 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg"></img>
+    <br>
+    <div class="row">
+      <div class="col">
+        <h3>Proprietaire</h3>
+        jgfjhgejrfle
+        <br>
+        <q-btn color="primary" icon="warning" label="Join" @click="login" class="q-ma-md"></q-btn>
+
+      </div>
+      <div class="col">
+        <h3>Locataire</h3>
+        jbfkjgebjgkek
+        <br>
+        <q-btn color="primary" icon="warning" label="Join" @click="abandon" class="q-ma-md"></q-btn>
+
+      </div>
+    </div>
   </q-page>
 </template>
 
