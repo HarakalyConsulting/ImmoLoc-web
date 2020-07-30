4707401f72838e2fb3bcab4eb093a8b584329595
Hary <hary@quinpex.io>
2020-07-30 16:50:27 +0200

    bludenie

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,49 +1,63 @@
-
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
 
-export default
-{
+export default {
   name: 'MainLayout',
-  components:
-  {
+  components: {
     EssentialLink
   },
-    data()
-    {
-        return {
-            leftDrawerOpen: false,
-            essentialLinks:
-            [
-                {title: 'Dashboard', caption: 'quasar.dev', icon: 'school', link: 'https://quasar.dev'}
-            ]
-        };
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: 'Dashboard',
+          caption: 'quasar.dev',
+          icon: 'school',
+          link: 'https://quasar.dev'
+        },
+        {
+          title: 'Dashboarddd',
+          caption: 'quasar.dev',
+          icon: 'school',
+          link: 'https://quasar.dev'
+        }
+      ]
     }
+  }
 }
 </script>
 
 <template>
   <q-layout view="lHh Lpr lFf">
-
     <q-header name="header">
       <q-toolbar class="bg-grey-8">
-        <q-btn flat dense round icon="menu" aria-label="Menu" @click="leftDrawerOpen = !leftDrawerOpen"/>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
         <q-toolbar-title>APP</q-toolbar-title>
-        <div>Quasar v{{ head.name }}</div>
+        <div>Quasar v{{ $name }}</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" content-class="bg-grey-9 text-white">
       <q-list>
-        <q-item-label header class="text-grey-1">Essential Links</q-item-label>
-        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link"/>
+        <!-- <q-item-label header class="text-grey-1">Essential Links</q-item-label> -->
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
       </q-list>
     </q-drawer>
 
     <q-page-container>
-      <router-view/>
+      <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
-
--- src/pages/owner/Profile.vue
+++ src/pages/owner/Profile.vue
@@ -1,54 +1,5 @@
-<template>
-  <q-page class="flex flex-center">
-    <div>
-      <div class="text-h6">Profile proprietaire</div>
-      <div class="q-gutter-md row items-start">
-        <q-input v-model="a.lastName" label="Nom" />
-        <q-input v-model="a.firstName" label="Prenom" />
-      </div>
-
-      <div class="text-h6">Address</div>
-      <div class="q-gutter-md row items-start">
-        <q-input v-model="a.rue" label="Rue" />
-        <q-input v-model="a.codePostal" label="Code Postal" />
-        <q-input v-model="a.ville" label="Ville" />
-        <q-input v-model="a.pays" label="Pays" />
-      </div>
-
-      <div class="text-h6">Details Bancaires</div>
-      <div class="q-gutter-md row items-start">
-        <q-input v-model="iban" label="IBAN" mask="#### #### #### ####" />
-      </div>
-
-      <div class="q-py-md">
-        <q-table
-          title="Proprietes"
-          :data="tabledata"
-          :columns="columns"
-          row-key="name"
-        />
-      </div>
-
-      <q-btn
-        color="primary"
-        icon="warning"
-        label="Abandon"
-        to="/"
-        class="q-ma-md"
-      ></q-btn>
-      <q-btn
-        color="red"
-        icon="error"
-        label="Enregister"
-        @click="save"
-        :loading="Submitting"
-        class="q-ma-md"
-      ></q-btn>
-    </div>
-  </q-page>
-</template>
-
 <script>
+import MainLayout from '../../layouts/MainLayout.vue';
 export default {
   name: 'PageIndex',
   data: function() {
@@ -91,10 +42,10 @@ export default {
     }
   },
   methods: {
-    save({ commit }, a) {
-      alert('Donnees enregistrees')
-      header.name="kkkk";
-      let login = axios.post('api/auth/register', form).then(response => {
+    save({ commit }, a) {      alert('Donnees enregistrees')
+      // <MainLayout name="llll"></MainLayout>
+
+      axios.post('api/auth/register', form).then(response => {
         commit('login', {
           token: response.data.token,
           user: response.data.user
@@ -105,3 +56,56 @@ export default {
   }
 }
 </script>
+
+
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <div class="text-h6">Profile proprietaire</div>
+      <div class="q-gutter-md row items-start">
+        <q-input v-model="a.lastName" label="Nom" />
+        <q-input v-model="a.firstName" label="Prenom" />
+      </div>
+
+      <div class="text-h6">Address</div>
+      <div class="q-gutter-md row items-start">
+        <q-input v-model="a.rue" label="Rue" />
+        <q-input v-model="a.codePostal" label="Code Postal" />
+        <q-input v-model="a.ville" label="Ville" />
+        <q-input v-model="a.pays" label="Pays" />
+      </div>
+
+      <div class="text-h6">Details Bancaires</div>
+      <div class="q-gutter-md row items-start">
+        <q-input v-model="iban" label="IBAN" mask="#### #### #### ####" />
+      </div>
+
+      <!-- <div class="q-py-md"> -->
+      <div class="q-pa-sm">
+        <q-table
+          title="Proprietes"
+          :data="tabledata"
+          :columns="columns"
+          row-key="name"
+        />
+      </div>
+
+      <q-btn
+        color="primary"
+        icon="warning"
+        label="Abandon"
+        to="/"
+        class="q-ma-md"
+      ></q-btn>
+      <q-btn
+        color="red"
+        icon="error"
+        label="Enregister"
+        @click="save"
+        :loading="Submitting"
+        class="q-ma-md"
+      ></q-btn>
+    </div>
+  </q-page>
+</template>
+
