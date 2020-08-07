f7800b09851f0366a827411289dff4e9333916f5
Hary <hary@quinpex.io>
2020-08-07 08:13:19 +0200

    added property.vue page

---
+++ src/pages/owner/Property.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="q-mt-lg q-pa-md bg-grey-2">
+    <div>
+      <div class="text-h6">Profile Locataire</div>
+
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "Property"
+}
+</script>
+
+<style scoped>
+
+</style>
