58ef99b4270753b753f2dd8b839f512f60f6c8c5
Hary <hary@quinpex.io>
2020-08-14 17:53:10 +0200

    WiP

--- src/pages/owner/EdL.vue
+++ src/pages/owner/EdL.vue
@@ -1,24 +1,20 @@
 <template>
-  <section :class="edl" style="max-width: 350px">
-    <q-page class="q-mt-lg q-pa-md bg-grey-2">
-      <div>
-        <div class="text-h6">Etats des Lieux</div>
-        <div class="q-gutter-md row items-start">
-          <!--          <q-input label="Nom" v-model="edl.name" readonly/>-->
-          <!--          &lt;!&ndash;  TODO:      new line here .... &ndash;&gt;-->
-          <!--          <q-input label="Rue" v-model="edl.street" readonly/>-->
-          <!--          <q-input label="Post Code" v-model="edl.codePostal" readonly/>-->
-          <!--          <q-input label="Ville" v-model="edl.town" readonly/>-->
-          <!--          <q-input label="Pays" v-model=".country" readonly/>-->
-
-          <div v-for="(value, name) in this.edl">
-            <q-input v-bind:label="value" v-model="avalue" readonly/>
+  <!--  <section :class="edl" style="max-width: 350px">-->
+  <q-page class="q-mt-lg q-pa-md bg-grey-2">
+    <div>
+      <div class="text-h6">Etats des Lieux</div>
+      <div class="q-gutter-md col items-start">
+        <div v-for="r in this.edl">
+          <div v-for="(value, name) in r">
+            <q-select v-bind:label="name" v-model="r.value" :options="options"
+                      @click="typeSelect" style="min-width: 100px"/>
           </div>
-
         </div>
+
       </div>
-    </q-page>
-  </section>
+    </div>
+  </q-page>
+  <!--  </section>-->
 </template>
 
 <script>
@@ -34,7 +30,8 @@ export default {
     return {
       app: [],
       edl: [],
-      avalue: ''
+      avalue: '',
+      options: ['Tres bon', 'Bon', 'Use', 'Sale', 'Tres sale']
     }
   },
   // props: {
@@ -43,19 +40,8 @@ export default {
   components: {
     'room-component': roomComponent
   },
-  // methods: {
-  //   edls() {
-  //     this.apptId = this.$route.params.id;
-  //     this.$axios.get('http://localhost:8888/api/v1/edl/' + this.apptId)
-  //       .then(response => {
-  //         this.edl = response.data.edl;
-  //       })
-  //       .catch(err => alert(err));
-  //     return this.edl;
-  //   }
-  // },
   created() {
-    //  take EDL id from the path
+    //  take appart id from the path
     this.apptId = this.$route.params.id;
     //this.$route.fullPath
     //  load user data at opening of the page ....
