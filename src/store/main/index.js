a0543d2abd96df79ec1f4f8b1537390b1a48a3b7
Hary <hary@quinpex.io>
2020-08-21 16:37:21 +0200

    Added 'token' to store

--- src/store/main/index.js
+++ src/store/main/index.js
@@ -1,29 +1,30 @@
-
 const store = {namespaced: true};
 
 store.state = {
-    toolbarMessage: ''
+  toolbarMessage: '',
+  token: ''
 };
 
 store.getters = {
 
-    getToolbarMessage: function(state)
-    {
-        return state.toolbarMessage;
-    }
-
+  getToolbarMessage: function (state) {
+    return state.toolbarMessage;
+  },
+  getToken: function (state) {
+    return state.token;
+  }
 };
 
-store.actions = {
-};
+store.actions = {};
 
 store.mutations = {
 
-    toolbarMessage: function(state, value)
-    {
-        state.toolbarMessage = value;
-    }
-
+  toolbarMessage: function (state, value) {
+    state.toolbarMessage = value;
+  },
+  token: function(state, value){
+    state.token = value;
+  }
 };
 
 export default store;
